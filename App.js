7caac662f2ceec0e584dd0cdc2876544d75ac16c
Javier Alaves <jayalaves@gmail.com>
2023-11-05 00:42:42 +0100

    basic design

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import Button from './src/components/Button';
+import ImageViewer from './src/components/ImageViewer';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.imageContainer}>
+				<ImageViewer placeholderImageSource={PlaceholderImage} />
+			</View>
+			<View style={styles.footerContainer}>
+				<Button theme='primary' label='Choose photo' />
+				<Button label='Use this photo' />
+			</View>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#25292e',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	imageContainer: {
+		flex: 1,
+		paddingTop: 58,
+	},
+	footerContainer: {
+		flex: 1 / 3,
+		alignItems: 'center',
+	},
 });
